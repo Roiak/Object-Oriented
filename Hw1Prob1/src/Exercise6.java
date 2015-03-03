b856322b6807e377d7f5c7040618a077ce3e3e71
roiak <rkarlinsky@bennington.edu>
2015-03-03 12:03:30 -0500

    "Problem 6 now done too"

---
+++ Hw1Prob1/src/Exercise6.java
@@ -0,0 +1,20 @@
+/**
+ * Created by rkarlinsky on 3/3/15.
+ * Working with storage(). Not exactly sure what to do here.
+ * I used the code from the book that should hold in storage
+ * the number of characters in a string that's fed in main.
+ * can't get it to print so I know it works though.
+ */
+public class Exercise6 {
+
+    public static int storage(String x) {
+        int stat = x.length()*2;
+        return stat;
+
+    }
+// this is the problem here
+    public static void printS(){
+        System.out.println(storage());
+    }
+}
+
--- Hw1Prob1/src/Main.java
+++ Hw1Prob1/src/Main.java
@@ -3,6 +3,13 @@
  */
 public class Main {
     public static void main(String[] args) {
-            Initializing1.integer(); Initializing1.character();
+        // first instances called are for the initializing shiz:
+
+        //Initializing1.integer(); Initializing1.character();
+
+        // this is for the storage bit, which I think just returns
+        // an integer but I can't get output:
+
+        // Exercise6.storage("Dude I like it");
         }
 }
