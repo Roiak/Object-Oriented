549c3f451aca7e27861df5662a7f4f60c333f109
roiak <rkarlinsky@bennington.edu>
2015-03-03 11:36:37 -0500

    "Exercise 1 done bee"

---
+++ Hw1Prob1/src/Initializing1.java
@@ -0,0 +1,19 @@
+/**
+ * Created by rkarlinsky on 3/3/15.
+ *This class will initialize an integer and a character
+ * by default
+ */
+
+public class Initializing1 {
+
+    public static void integer(){
+        int i = 5;
+        System.out.println(i);
+    }
+    public static void character(){
+        char d = 25;
+        System.out.println(d);
+    }
+
+
+}
---
+++ Hw1Prob1/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by rkarlinsky on 3/2/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+            Initializing1.integer(); Initializing1.character();
+        }
+}
